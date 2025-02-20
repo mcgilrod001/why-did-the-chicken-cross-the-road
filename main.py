2e0c18b7106c58be4d2aa6c6637fc4b1d03c36b2
Roddy <126286319+mcgilrod001@users.noreply.github.com>
2025-02-20 18:11:58 +0000

    dasdasdsada

---
+++ main.py
@@ -0,0 +1,16 @@
+# root setup
+import customtkinter as ctk
+
+
+Root = ctk.CTk()
+
+# general config
+ctk.set_default_color_theme('dark-blue')
+ctk.set_appearance_mode('dark')
+Root.geometry("700x300")
+Root.title("heehehhehehehe")
+
+
+
+Root.mainloop()
+
